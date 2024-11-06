12c0b68a51d447d41bfeb8c16795834712efcc98
JSGGYU00 <seonggyu426@gmail.com>
2024-11-06 22:22:15 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long a, b;
+
+    cin >> a >> b;
+
+    cout << abs(a - b) << endl;
+       
+    return 0;
+}

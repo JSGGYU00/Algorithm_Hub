733d0f90411e294299c42c780cb4f82ff4f2ea96
JSGGYU00 <seonggyu426@gmail.com>
2024-11-06 22:42:26 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cin >> a >> b;
+
+    for (int i = 0; i < a; i++)
+    {
+        int c, d;
+        cin >> c >> d;
+    }
+    
+    cout << "비와이" << endl;
+
+    return 0;
+}
